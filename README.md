8a4dab86391433ee34279cfdaad19a2e7f1c7f6f
anosora233 <feixiangs@outlook.com>
2023-07-15 13:08:01 +0800

    Rename project name

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# soul-helper
+# richi-console
+
+基于 [mahjong-helper-majsoul-mitmproxy](https://github.com/Avenshy/mahjong-helper-majsoul-mitmproxy) 的懒人版打包
+
+目前仅支持 Windows 10/11 64 位系统
+
+## Windows 运行
+
+1. 转到 [Releases]() 下载压缩包并解压。
+
+2. 运行 *richi-console.exe*
+
+3. 在浏览器访问 [mitm.it](http://mitm.it) 并按提示安装证书，之后即可在浏览器正常游戏
+
+4. 为确保系统代理正确重置，退出请直接关闭 *alacritty* 窗口
+
+## 使用 http 前置代理
+
+如果你有使用代理软件，并且希望通过代理进行游戏
+
+可以修改 *bin\settings.json* 中 *UPSTREAM_PROXY* 项，留空为不使用代理
+
+*e.g.* `"UPSTREAM_PROXY": "127.0.0.1:2080"`
+
+## 特别感谢
+
+- [Avenshy/mahjong-helper-majsoul-mitmproxy](https://github.com/Avenshy/mahjong-helper-majsoul-mitmproxy)
+
+- [EndlessCheng/mahjong-helper](https://github.com/EndlessCheng/mahjong-helper)
+
+- [alacritty/alacritty](https://github.com/alacritty/alacritty)
---
+++ src/addons.py
@@ -0,0 +1,82 @@
+import json
+import logging
+import requests
+import mitmproxy.http
+import richi
+import pb2 as pb
+
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+from google.protobuf.json_format import MessageToDict
+from base64 import b64decode
+
+# 导入配置
+SETTINGS = json.load(open("bin/settings.json", "r"))
+
+SEND_METHOD = SETTINGS["SEND_METHOD"]  # 需要发送给小助手的method
+SEND_ACTION = SETTINGS["SEND_ACTION"]  # '.lq.ActionPrototype'中，需要发送给小助手的action
+API_URL = SETTINGS["API_URL"]  # 小助手的地址
+UPSTREAM_PROXY = SETTINGS["UPSTREAM_PROXY"]
+
+logging.info(
+    f"""
+    已载入配置：\n
+    SEND_METHOD: {SEND_METHOD}\n
+    SEND_ACTION: {SEND_ACTION}\n
+    API_URL: {API_URL}
+    UPSTREAM_PROXY: {UPSTREAM_PROXY}
+    """
+)
+
+richi_proto = richi.RichiProto()
+# 禁用urllib3安全警告
+disable_warnings(InsecureRequestWarning)
+
+
+class WebSocketAddon:
+    def websocket_message(self, flow: mitmproxy.http.HTTPFlow):
+        # 在捕获到WebSocket消息时触发
+        assert flow.websocket is not None  # make type checker happy
+        message = flow.websocket.messages[-1]
+        # 解析proto消息
+        result = richi_proto.parse(message)
+        if message.from_client is False:
+            logging.info(f"接收到：{result}")
+        if result["method"] in SEND_METHOD and message.from_client is False:
+            if result["method"] == ".lq.ActionPrototype":
+                if result["data"]["name"] in SEND_ACTION:
+                    data = result["data"]["data"]
+                    if result["data"]["name"] == "ActionNewRound":
+                        # 雀魂弃用了md5改用sha256，但没有该字段会导致小助手无法解析牌局，也不能留空
+                        # 所以干脆发一个假的，反正也用不到
+                        data["md5"] = data["sha256"][:32]
+                else:
+                    return
+            elif result["method"] == ".lq.FastTest.syncGame":  # 重新进入对局时
+                actions = []
+                for item in result["data"]["game_restore"]["actions"]:
+                    if item["data"] == "":
+                        actions.append({"name": item["name"], "data": {}})
+                    else:
+                        b64 = b64decode(item["data"])
+                        action_proto_obj = getattr(pb, item["name"]).FromString(b64)
+                        action_dict_obj = MessageToDict(
+                            action_proto_obj,
+                            preserving_proto_field_name=True,
+                            including_default_value_fields=True,
+                        )
+                        if item["name"] == "ActionNewRound":
+                            # 这里也是假md5，理由同上
+                            action_dict_obj["md5"] = action_dict_obj["sha256"][:32]
+                        actions.append({"name": item["name"], "data": action_dict_obj})
+                data = {"sync_game_actions": actions}
+            else:
+                data = result["data"]
+            logging.warn(f"已发送：{data}")
+            requests.post(API_URL, json=data, verify=False)
+            if "liqi" in data.keys():  # 补发立直消息
+                logging.warn(f'已发送：{data["liqi"]}')
+                requests.post(API_URL, json=data["liqi"], verify=False)
+
+
+addons = [WebSocketAddon()]
