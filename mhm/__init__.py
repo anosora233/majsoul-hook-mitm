11174410f1e66dd6223748d0f947e46ecb0d0ed4
anosora233 <86921203+anosora233@users.noreply.github.com>
2024-03-25 22:43:54 +0800

    chore: Remove assets and update protobuf
    In this commit:
    - Removed assets which are no longer needed.
    - Updated mhm/proto/*_pb2.py to reflect changes related to protobuf.
    - Updated requirements.txt with necessary dependencies.

--- mhm/__init__.py
+++ mhm/__init__.py
@@ -1,9 +1,3 @@
-import os
-
-from google.protobuf import __version__
 from rich.console import Console
-
-if __version__.split(".") > ["3", "18", "0"]:
-    os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 console = Console()
